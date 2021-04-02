efc29f4f05698423f88502cf2d9e7807b9c48a6a
nyc0lai <nyc0lai@gmail.com>
2021-04-02 13:38:22 +0300

    you officially speak whale!

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-const input = "balena albastra";
+const input = "A smile is the best make-up";
 
 const vowels = ["a", "e", "i", "o", "u"];
 
-///const resultArray = 
-let resultArray = [];
+const resultArray = [];
 
 for(let i = 0; i < input.length; i++) {
 
    for(let j = 0; j < vowels.length; j++) {
+
       if (input[i] === vowels[j])
       resultArray.push(vowels[j]);
   }
- }
- console.log(resultArray);
+if(input[i] === "e" || input[i] === "u")
+resultArray.push(input[i]);
+
+}
+
+console.log(resultArray.join('').toUpperCase());
